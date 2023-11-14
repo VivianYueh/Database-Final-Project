049e9d64e5a50cef0a629d7ea30f149f671de071
VivianYueh <100284513+VivianYueh@users.noreply.github.com>
2023-11-14 23:49:44 +0800

    Update item_mdysave.php

--- item_mdysave.php
+++ item_mdysave.php
@@ -24,32 +24,42 @@
 <div class="container1">
     <button type="button" onclick="location.href='item_add.php'">新增</button>
     <table>
-        <thead>
-            <tr>
-                <th></th>
-                <th>#</th>
-                <th>物品</th>
-                <th>價錢</th>
-                <th>物品敘述</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td><button type="button">我要更改</button></td>
-                <td rowspan="2"><input type="text" id="num" name="num" value="1"></td>
-                <td rowspan="2"><input type="text" id="item_name" name="item_name" value="馬克杯"></td>
-                <td rowspan="2"><input type="text" id="item_cost" name="item_cost" value="100"></td>
-                <td rowspan="2"><input type="text" id="item_des" name="item_des" value="大象圖案的白色馬克杯"></td>
-            </tr>
-            <tr>
-                <td><button type="button">我要刪除</button></td>
-            </tr>
-        </tbody>
+        <form action="" method="post">
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>#</th>
+                    <th>物品</th>
+                    <th>價錢</th>
+                    <th>物品敘述</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><input type="submit" name="submit-btn1" value="我要更改" /></button></td>
+                    <td rowspan="2"><input type="text" id="num" name="num" value="1"></td>
+                    <td rowspan="2"><input type="text" id="item_name" name="item_name" value="馬克杯"></td>
+                    <td rowspan="2"><input type="text" id="item_cost" name="item_cost" value="100"></td>
+                    <td rowspan="2"><input type="text" id="item_des" name="item_des" value="大象圖案的白色馬克杯"></td>
+                </tr>
+                <tr>
+                    <td><input type="submit" name="submit-btn2" value="我要刪除" /></td>
+                </tr>
+            </tbody>
+        </form>
+        
         
     </table>
 </div>
 
+<?php
+if (isset($_POST["submit-btn1"])) {
+    # 依欄位名稱取得資料
+    echo $_POST["item_name"];
+}
 
+  
+?>
 
 
 
